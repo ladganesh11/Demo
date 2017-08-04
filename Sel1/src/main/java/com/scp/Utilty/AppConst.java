b64faac1d7089ee3c3429e5c7168835f2265d1be
Ganesh <Ganesh@Masters>
2017-08-04 12:43:02 +0530

    yes

---
+++ Sel1/src/main/java/com/scp/Utilty/AppConst.java
@@ -0,0 +1,8 @@
+package com.scp.Utilty;
+
+public class AppConst {
+	public static String APPLICATION_URL="http://automationpractice.com/index.php?controller=authentication&back=my-account";
+	 public static String Applcation_Url ="http://automationpractice.com/index.php?controller=authentication&back=my-account";
+	//public static String Applcation_Url=""http://automationpractice.com/index.php?controller=authentication&back=my-account";
+
+}
