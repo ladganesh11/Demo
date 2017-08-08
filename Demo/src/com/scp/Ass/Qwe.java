1a4baf2405a39143c82c615ecb544cada80bcd00
Ganesh <Ganesh@Masters>
2017-08-08 11:55:07 +0530

    first commit

---
+++ Demo/src/com/scp/Ass/Qwe.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.scp.Ass;
+
+/**
+ * @author Ganesh
+ *
+ */
+public class Qwe {
+
+	/**
+	 * 
+	 */
+	public Qwe() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello word ");
+		
+
+	}
+
+}
